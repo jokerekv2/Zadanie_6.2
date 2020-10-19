b3f0880921946ebd6f022622b68aee4682de11d2
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-10-19 19:21:40 +0200

    poprawa rozwiązania

--- src/Point.java
+++ src/Point.java
@@ -1,7 +1,6 @@
 public class Point {
     private int x;
     private int y;
-    private String quadrant;
 
     public Point(int x, int y) {
         this.x = x;
@@ -9,7 +8,9 @@ public class Point {
     }
 
     public String whatQuarter() {
-        if (x > 0  && y > 0)
+        if (x == 0 && y ==0)
+            return "0";
+        else if (x > 0  && y > 0)
             return "I";
         else if (x > 0 && y < 0)
             return "IV";
@@ -37,6 +38,9 @@ public class Point {
 
     @Override
     public String toString() {
+        if (whatQuarter() == "0")
+            return "Point ("+ x + ", " + y +") is at the origin of the coordinate system.";
+        else
         return "Point ("+ x + ", " + y +") is in the " + whatQuarter() + " quadrant." ;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        Point point = new Point(5, -3);
+        Point point = new Point(0, 0);
         Point point1 = new Point(-1, 3);
         System.out.println(point.toString());
         System.out.println(point1.toString());
